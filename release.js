4cb7bcf4e4bc99eaefa5614bcdf9252d223d499d
Jeremie Rodriguez <contact@jeremierodriguez.com>
2020-04-03 20:44:30 +0200

    chore: move release script from semver to calver

--- release.js
+++ release.js
@@ -7,39 +7,42 @@ const exec = promisify(require('child_process').exec);
 const writeFile = promisify(require('fs').writeFile);
 
 const packageJSONPath = path.resolve(__dirname, 'package.json');
-const version = process.argv[2];
 
 function exit(message) {
   console.log(message);
   process.exit(1);
 }
 
-if (!version) exit('Version must be passed as parameter');
-if (!version.match(/^(\d+\.){2}\d+$/)) exit('Version must match /^(\\d+\\.){2}\\d+$/');
+async function computeNextVersion() {
+  const { stdout } = await exec('git describe --abbrev=0');
+  const [major, minor, patch] = stdout.split('.').map(s => parseInt(s, 10));
+  const date = new Date();
+  const nextMajor = parseInt(date.getFullYear().toString().substring(2, 4), 10);
+  const nextMinor = date.getMonth() + 1;
+  const nextPatch = nextMajor === major && nextMinor === minor ? patch + 1 : 0;
+  const parts = [nextMajor.toString().padStart(2, 0), nextMinor.toString().padStart(2, 0), nextPatch];
+  return parts.join('.');
+}
 
 (async () => {
   try {
-    // 1. Go on master branch & rebase on develop
+    // 1. Compute next version using calendar versioning
+    const version = await computeNextVersion();
+
+    // 2. Go on master branch & rebase on develop
     console.log('Checkout master branch');
     await exec('git checkout master');
     console.log('Rebase master branch on develop');
     await exec('git rebase develop');
 
-    // 2. Bump package.json & update package-lock.json
+    // 3. Bump package.json & update package-lock.json
     console.log('Bump package.json version');
-    const newPackageJSON = `${JSON.stringify(
-      {
-        ...packageJSON,
-        version,
-      },
-      undefined,
-      2,
-    )}${EOL}`;
+    const newPackageJSON = `${JSON.stringify({ ...packageJSON, version }, undefined, 2)}${EOL}`;
     await writeFile(packageJSONPath, newPackageJSON);
     console.log('Update package-lock.json');
     await exec('npm install');
 
-    // 3. Create release commit, tag it & push it to remote
+    // 4. Create release commit, tag it & push it to remote
     console.log('Create release commit');
     await exec('git add package.json package-lock.json');
     await exec(`git commit -m 'chore: release v${version}'`);
@@ -48,7 +51,7 @@ if (!version.match(/^(\d+\.){2}\d+$/)) exit('Version must match /^(\\d+\\.){2}\\
     console.log('Push new master version');
     await exec('git push --follow-tags');
 
-    // 4. Go back to develop branch, rebase on released master & push it to remote
+    // 5. Go back to develop branch, rebase on released master & push it to remote
     console.log('Checkout develop branch');
     await exec('git checkout develop');
     console.log('Rebase develop branch on master');
