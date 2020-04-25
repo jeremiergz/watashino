9dbe02d91e66743add8d191a253b9d95d523476c
Jeremie Rodriguez <contact@jeremierodriguez.com>
2020-04-25 13:37:11 +0200

    chore(scripts): add version validation step to release.js

--- release.js
+++ release.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const { EOL } = require('os');
 const path = require('path');
+const readline = require('readline');
 const { promisify } = require('util');
 const packageJSON = require('./package.json');
 const exec = promisify(require('child_process').exec);
@@ -29,25 +30,58 @@ async function computeNextVersion() {
   return parts.join('.');
 }
 
+function manuallyApproveVersion(version) {
+  return new Promise((resolve, reject) => {
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+    rl.question(`Release version ${version}? [Y/n] `, answer => {
+      const shouldContinue = !answer || answer.toLowerCase() === 'y';
+      rl.close();
+      if (shouldContinue) return resolve();
+      return reject(new Error('Release interrupted by user'));
+    });
+  });
+}
+
+function parseOptions() {
+  const args = process.argv.slice(2, process.argv.length);
+  return args.reduce(
+    (acc, arg) => {
+      if (arg === '-y' || arg === '--yes') acc.yes = true;
+      return acc;
+    },
+    {
+      yes: false, // Removes readline statements for automated environments
+    },
+  );
+}
+
 (async () => {
   try {
+    // 0. Get CLI options
+    const options = parseOptions();
+
     // 1. Compute next version using calendar versioning
     const version = await computeNextVersion();
 
-    // 2. Go on master branch & rebase on develop
+    // 2. Ask for version approval & continue if ok
+    if (!options.yes) {
+      await manuallyApproveVersion(version);
+    }
+
+    // 3. Go on master branch & rebase on develop
     console.log('Checkout master branch');
     await exec('git checkout master');
     console.log('Rebase master branch on develop');
     await exec('git rebase develop');
 
-    // 3. Bump package.json & update package-lock.json
+    // 4. Bump package.json & update package-lock.json
     console.log('Bump package.json version');
     const newPackageJSON = `${JSON.stringify({ ...packageJSON, version }, undefined, 2)}${EOL}`;
     await writeFile(packageJSONPath, newPackageJSON);
     console.log('Update package-lock.json');
     await exec('npm install');
 
-    // 4. Create release commit, tag it & push it to remote
+    // 5. Create release commit, tag it & push it to remote
     console.log('Create release commit');
     await exec('git add package.json package-lock.json');
     await exec(`git commit -m 'chore: release v${version}'`);
@@ -56,7 +90,7 @@ async function computeNextVersion() {
     console.log('Push new master version');
     await exec('git push --follow-tags');
 
-    // 5. Go back to develop branch, rebase on released master & push it to remote
+    // 6. Go back to develop branch, rebase on released master & push it to remote
     console.log('Checkout develop branch');
     await exec('git checkout develop');
     console.log('Rebase develop branch on master');
