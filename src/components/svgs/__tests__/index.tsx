b2d1bc7f4196673658693e32fcfb82f57a3a2b63
Jeremie Rodriguez <contact@jeremierodriguez.com>
2020-08-21 01:00:24 +0200

    refactor(svg): move icons to their specific folder

--- src/components/svgs/__tests__/index.tsx
+++ src/components/svgs/__tests__/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import A2HSIcon from '../A2HS';
-import BookIcon from '../Book';
-import BusinessIcon from '../Business';
-import CloseIcon from '../Close';
 import EmailIcon from '../contacts/Email';
 import FacebookIcon from '../contacts/Facebook';
 import GitHubIcon from '../contacts/GitHub';
@@ -16,103 +12,23 @@ import RedditIcon from '../contacts/Reddit';
 import SkypeIcon from '../contacts/Skype';
 import TwitterIcon from '../contacts/Twitter';
 import WhatsappIcon from '../contacts/Whatsapp';
-import CopyIcon from '../Copy';
-import DescriptionIcon from '../Description';
-import DownloadIcon from '../Download';
 import EmojiAstonishedIcon from '../emojis/Astonished';
 import EmojiNerdIcon from '../emojis/Nerd';
 import EmojiUglyIcon from '../emojis/Ugly';
-import HouseIcon from '../House';
-import MoodIcon from '../Mood';
-import MoonIcon from '../Moon';
-import PersonIcon from '../Person';
-import SunIcon from '../Sun';
+import A2HSIcon from '../icons/A2HS';
+import BookIcon from '../icons/Book';
+import BusinessIcon from '../icons/Business';
+import CloseIcon from '../icons/Close';
+import CopyIcon from '../icons/Copy';
+import DescriptionIcon from '../icons/Description';
+import DownloadIcon from '../icons/Download';
+import HouseIcon from '../icons/House';
+import MoodIcon from '../icons/Mood';
+import MoonIcon from '../icons/Moon';
+import PersonIcon from '../icons/Person';
+import SunIcon from '../icons/Sun';
 
 describe('components/svg Test Suite', () => {
-  describe('A2HS Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <A2HSIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Book Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <BookIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Business Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <BusinessIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Close Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <CloseIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Copy Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <CopyIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Description Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <DescriptionIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Download Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <DownloadIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('House Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <HouseIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Mood Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <MoodIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Moon Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <MoonIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Person Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <PersonIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
-  describe('Sun Test Suite', () => {
-    it('matches snapshot', () => {
-      const jsx = <SunIcon />;
-      expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
-    });
-  });
-
   describe('contacts Test Suite', () => {
     describe('Email Test Suite', () => {
       it('matches snapshot', () => {
@@ -216,6 +132,92 @@ describe('components/svg Test Suite', () => {
     describe('Ugly Test Suite', () => {
       it('matches snapshot', () => {
         const jsx = <EmojiUglyIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('icons Test Suite', () => {
+    describe('A2HS Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <A2HSIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('Book Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <BookIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('Business Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <BusinessIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('Close Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <CloseIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('Copy Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <CopyIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('Description Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <DescriptionIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('Download Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <DownloadIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('House Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <HouseIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('Mood Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <MoodIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('Moon Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <MoonIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('Person Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <PersonIcon />;
+        expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
+      });
+    });
+
+    describe('Sun Test Suite', () => {
+      it('matches snapshot', () => {
+        const jsx = <SunIcon />;
         expect(ReactTestRenderer.create(jsx).toJSON()).toMatchSnapshot();
       });
     });
