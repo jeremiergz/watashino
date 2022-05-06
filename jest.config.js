eaf4b6a19800501bfbbe81d9154f71b34aea68a6
Jeremie Rodriguez <contact@jeremierodriguez.com>
2022-05-06 17:28:39 -0400

    test: fix Jest config following update

--- jest.config.js
+++ jest.config.js
@@ -9,12 +9,14 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpe?g|png|gif|eot|otf|webp|svg|ttf|woff2?|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file.ts',
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^gatsby-page-utils/(.*)$': `gatsby-page-utils/dist/$1`,
+    '^gatsby-page-utils/(.*)$': `gatsby-page-utils/$1`,
   },
   setupFilesAfterEnv: ['<rootDir>/__mocks__/window.ts', '<rootDir>/jest.setup.js'],
   testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: 'http://localhost',
+  },
   testPathIgnorePatterns: ['node_modules', '\\.cache', '<rootDir>.*/public'],
-  testURL: 'http://localhost',
   transform: {
     '^.+\\.[jt]sx?$': '<rootDir>/jest-preprocess.js',
   },
