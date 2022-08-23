e06cdcb1c43716969b15ee6db109753816a43834
Jeremie Rodriguez <contact@jeremierodriguez.com>
2022-08-22 22:03:45 -0400

    test: update tests following update

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -1,7 +1,7 @@
 const babelJest = require('babel-jest').default;
 
 const babelOptions = {
-  presets: ['babel-preset-gatsby', '@babel/preset-typescript'],
+  presets: ['babel-preset-gatsby', ['@babel/preset-react', { runtime: 'automatic' }], '@babel/preset-typescript'],
 };
 
 module.exports = babelJest.createTransformer(babelOptions);
